efe3862d443bdbce2a306c7b6b72d4d90dcfb437
rominanayak <rominanayak30@gmail.com>
2016-02-09 12:36:05 -0500

    Adding the default asp.net web application project code

---
+++ blogging_portal/src/blogging_portal/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blogging_portal.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
